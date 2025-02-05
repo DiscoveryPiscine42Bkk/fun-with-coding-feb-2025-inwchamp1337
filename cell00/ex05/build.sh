921d74a5a0592372e03446a96180404c246fe8cb
inwchamp1337 <115950933+inwchamp1337@users.noreply.github.com>
2025-02-05 14:27:43 +0700

    update

--- cell00/ex05/build.sh
+++ cell00/ex05/build.sh
@@ -1,8 +1,14 @@
-for x in "$@"; do                 #vs $# 
+if [ "$#" -eq 0 ]; then
+    echo "No arguments supplied"
+    exit 1
+fi
+
+for x in "$@"; do
     if [[ ! -d "$x" ]]; then 
         mkdir "ex$x"
     fi
 done
+
 
 
 #-n "$x" &&  3
